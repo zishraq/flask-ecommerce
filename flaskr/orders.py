5dae9357531d4e93709c5b5e9536098c745fd206
zishraq <z.ishraq@gmail.com>
2022-01-18 09:51:01 +0600

    unnecessary codes removed

--- flaskr/orders.py
+++ flaskr/orders.py
@@ -12,7 +12,7 @@ bp = Blueprint('orders', __name__, url_prefix='/orders')
 
 @bp.route('/create-order', methods=['POST'])
 @login_required
-def create_shopping_cart():
+def create_order():
     response = {
         'isSuccess': False,
         'message': 'Create an Order'
@@ -56,51 +56,3 @@ def create_shopping_cart():
     response['isSuccess'] = True
     response['order_id'] = order_id
     return response
-
-
-@bp.route('/add-to-order/<cart_id>/', methods=['PUT'])
-@login_required
-def add_to_cart(cart_id):
-    response = {
-        'isSuccess': False,
-        'operation': 'Add to shopping cart'
-    }
-
-    product_name = request.get_json()['product_name']
-    quantity = request.get_json()['quantity']
-
-    if not product_name:
-        response['error'] = 'Product name is required.'
-        return response
-
-    else:
-        db = get_db()
-        db.execute(
-            'UPDATE shopping_cart SET product_name = ?, quantity = ?'
-            ' WHERE cart_id = ?',
-            (product_name, quantity, cart_id)
-        )
-        db.commit()
-        response['isSuccess'] = True
-
-    return response
-
-
-@bp.route('/get-all-carts', methods=['GET'])
-def get_all_carts():
-    db = get_db()
-    carts = db.execute(
-        'SELECT *'
-        ' FROM shopping_cart'
-        ' ORDER BY created_at ASC'
-    ).fetchall()
-
-    results = []
-
-    for i in carts:
-        results.append(dict(i))
-
-    return {
-        'isSuccess': True,
-        'posts': results
-    }
